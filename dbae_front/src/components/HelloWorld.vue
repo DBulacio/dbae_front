d791f23953718fb7a08c9b0680bf2f7d526b2c89
Daniel Angel Bulacio <daniel.bulacio@bodyhealthgroup.com>
2023-09-04 18:19:36 -0300

    login form

--- dbae_front/src/components/HelloWorld.vue
+++ dbae_front/src/components/HelloWorld.vue
@@ -1,20 +1,21 @@
 <script setup>
+import LoginForm from './LoginForm.vue'
 defineProps({
-  msg: {
+  title: {
     type: String,
     required: true
-  }
+  },
 })
 </script>
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+    <h1 class="green">{{ title }}</h1>
+    <div>
+      <LoginForm  />
+    </div>
+
+
   </div>
 </template>
 
---
+++ dbae_front/src/components/LoginForm.vue
@@ -0,0 +1,61 @@
+<script>
+  export default {
+    methods: {
+      onClick() {
+        this.btnText = 'Loading'
+        this.isDisabled = true
+      },
+    },
+    data() {
+      return {
+        btnText: 'Logi',
+        isDisabled: false,
+      }
+    }
+  }
+</script>
+
+
+<template>
+  <div class="greetings">
+    <form class="green">
+      <input type="user">
+      <input type="password">
+      <button @click="onClick()" :disabled="this.isDisabled" >{{ this.btnText  }}</button>
+    </form>
+
+
+    
+  </div>
+</template>
+
+<style scoped>
+form {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+}
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
